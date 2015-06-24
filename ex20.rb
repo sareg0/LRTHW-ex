9bf6ad95eed0b14d245da43d64b1881458e336cb
Sara Regan <regan.sara@gmail.com>
2015-06-24 17:34:50 +0200

    improvements to ex20

--- ex20.rb
+++ ex20.rb
@@ -2,18 +2,18 @@
 # i.e. a file
 input_file = ARGV.first
 
-# here we create a function with the argument f which is the file, and printing on the following lines
+# here we create a function with the argument f which will be the file, and printing on the following lines
 def print_all(f)
   puts f.read
 end
 
-# here we are defining the function 'rewind' with the arugment f (which is the file). We then use the helper method
-# .seek on the argument to jump to the position '0'
+# here we are defining the function 'rewind' with the arugment f (which will be the file). We then use the ruby method
+# .seek on the argument 'f' to jump to the position '0'
 def rewind(f)
   f.seek(0)
 end
 
-# here we define the method 'print_a_line' on the arguments. and you give an input for 'f' from the file
+# here we define the method 'print_a_line' on the arguments 'line_count' and 'f'. and you give an input for 'f' from the file
 def print_a_line(line_count, f)
   puts "#{line_count}, #{f.gets.chomp}"
 end
@@ -23,7 +23,7 @@ current_file = open(input_file)
 
 puts "First let's print the whole file:\n"
 
-# here we run the function 'print_all', which prints the current file
+# here we run the function 'print_all', which prints the 'current file' variable
 print_all(current_file)
 
 puts "Now let's rewind, kind of like a tape."
@@ -36,21 +36,31 @@ puts "Let's print three lines:"
 
 # here you define the current line as '1'
 current_line = 1
-# here you print the current line
+# here you print the current line (currently 1) of the file assigned to 'current_file' variable
 print_a_line(current_line, current_file)
 
-# here you increase the line count by one
+# here you increase the line count by one, making it 2
 current_line = current_line += 1
-# here you print the next line, and the same follows again on the following lines
+# here you print the 'current_line' which is now '2', of the 'current_file'
 print_a_line(current_line, current_file)
 
 current_line = current_line += 1
+# here you print the 'current_line' which is now '3', of the 'current_file'.
 print_a_line(current_line, current_file)
 
 
-# study drills
-#1.Write English comments for each line to understand what that line does.
-#2.Each time print_a_line is run, you are passing in a variable current_line. Write out what current_line is equal to on each function call, and trace how it becomes line_count in print_a_line.
-#3.Find each place a function is used, and check its def to make sure that you are giving it the right arguments.
-#4.Research online what the seek function for file does. Try ri File and see if you can figure it out from there.
-#5.Research the shorthand notation += and rewrite the script to use += instead.
+=begin
+ it is important to notw the the two arguments being passed to 'print_a_line'
+ in lines 40, 45, and 49, are the the values replacing the arguments 'line_count'
+ and 'f' in line 17.
+
+study drills
+1.Write English comments for each line to understand what that line does.
+2.Each time print_a_line is run, you are passing in a variable current_line. Write out what current_line is equal to on each function call, and trace how it becomes line_count in print_a_line.
+3.Find each place a function is used, and check its def to make sure that you are giving it the right arguments.
+4.Research online what the seek function for file does. Try 'ri File' and see if you can figure it out from there.
+seek is a method of the IO class in Ruby. IO is the basis for all input/output in Ruby
+seek moves the file pointer (set automatically at the beginning of a file)
+to the integer given in the first paramater.
+5.Research the shorthand notation += and rewrite the script to use += instead.
+=end
